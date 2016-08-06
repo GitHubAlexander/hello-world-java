4296cc76d3b27f1078a5a8bb062768c605231b09
Alexander <leitner.alexander@yahoo.com>
2016-08-06 19:05:10 +0200

    v1.0.2

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -8,6 +8,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         System.out.println("Git");
         System.out.println("Window");
+        System.out.println("Tommy");
     }
     
 }
